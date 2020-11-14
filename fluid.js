dd23cb7b3d1f9d3b75c60784734f65fc9ee3fd91
Cesar Zapata <czapata@hugeinc.com>
2020-11-13 23:49:33 -0500

    Move all CSS to different component modules

--- fluid.js
+++ fluid.js
@@ -3,16 +3,19 @@ var expandNav = document.querySelector("#expand-nav");
 var contrNav = document.querySelector("#contr-nav");
 
 function openNav() {
-  sidenav.style.height = "100%";
-  sidenav.style.paddingTop = "60px";
-  document.querySelector(".closebtn");
-  expandNav.style.display = "none";
-  contrNav.style.display = "block";
+    sidenav.style.height = "100%";
+    sidenav.style.paddingTop = "60px";
+    document.querySelector(".closebtn");
+    expandNav.style.display = "none";
+    contrNav.style.display = "block";
 }
 
 function closeNav() {
-  sidenav.style.height = "0px";
-  sidenav.style.paddingTop = "0px";
-  expandNav.style.display = "block";
-  contrNav.style.display = "none";
+    sidenav.style.height = "0px";
+    sidenav.style.paddingTop = "0px";
+    expandNav.style.display = "block";
+    contrNav.style.display = "none";
 }
+
+
+console.log('🎉 Fluid is running. \n 👉 Check more in https://github.com/nikhilmwarrier/fluidcss')
