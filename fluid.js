4059eed64767f3222421fda96ed543db7feb64c1
Nikhil M Warrier <70085759+nikhilmwarrier@users.noreply.github.com>
2020-11-17 07:32:03 +0000

    Add files via upload

--- fluid.js
+++ fluid.js
@@ -1,6 +1,8 @@
 var sidenav = document.querySelector("#sidenav");
 var expandNav = document.querySelector("#expand-nav");
 var contrNav = document.querySelector("#contr-nav");
+var root = document.documentElement;
+var body = document.body;
 
 function openNav() {
   sidenav.style.height = "100%";
@@ -16,3 +18,25 @@ function closeNav() {
   expandNav.style.display = "block";
   contrNav.style.display = "none";
 }
+
+//! Fix for 100vw overflow-x issue
+// Creating invisible container
+const outer = document.createElement("div");
+outer.style.visibility = "hidden";
+outer.style.overflow = "scroll"; // forcing scrollbar to appear
+outer.style.msOverflowStyle = "scrollbar"; // needed for WinJS apps
+document.body.appendChild(outer);
+
+// Creating inner element and placing it in the container
+const inner = document.createElement("div");
+outer.appendChild(inner);
+
+// Calculating difference between container's full width and the child width
+const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
+
+// Removing temporary elements from the DOM
+outer.parentNode.removeChild(outer);
+
+root.style.setProperty("--default-scrollbar-width", scrollbarWidth + "px");
+console.log(scrollbarWidth);
+//! END//
